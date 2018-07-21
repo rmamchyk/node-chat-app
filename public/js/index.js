f665329df48e3e0e6dc061c6f4066c5a215470f3
rmamchyk <roman.mamchyk@gmail.com>
2018-07-21 13:33:45 +0300

    Scroll to bottom if user is closed to buttom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to server');
 });
@@ -17,6 +33,7 @@ socket.on('newMessage', function(message){
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -28,6 +45,7 @@ socket.on('newLocationMessage', function(message){
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e){
